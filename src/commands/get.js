b697b9689231a89c5966764adb92db90d6fdedd9
Provant <t6@live.com.au>
2023-11-01 00:21:35 +1000

    not currently working suspected interactionCreate

--- src/commands/get.js
+++ src/commands/get.js
@@ -64,75 +64,69 @@ module.exports = {
       ],
     });
 
-    // Listen for the interaction with the select menu
-    client.on("interactionCreate", async (interaction) => {
-      if (!interaction.isStringSelectMenu()) return;
+    if (!interaction.isStringSelectMenu()) return;
 
-      // Check if the select menu interaction is for choosing a pet
-      if (interaction.customId === "getCreateMenu") {
-        // Get the selected pet type from the interaction
-        const selectedPetType = interaction.values[0]; // Assuming only one option can be selected
-        console.log("Selected Pet Type:", selectedPetType);
+    if (interaction.customId === "getCreateMenu") {
+      const selectedPetType = interaction.values[0];
+      console.log("Selected Pet Type:", selectedPetType);
 
-        // Save the selected pet type to the database
-        try {
-          await userModel
-            .findOneAndUpdate(
-              { userId: interaction.user.id },
-              { petType: selectedPetType },
-              { upsert: true }
-            )
-            .exec();
-          console.log("Saved Pet Type to Database:", selectedPetType);
-        } catch (error) {
-          console.error("Error updating pet type:", error);
-          interaction.reply({
-            content:
-              "An error occurred while processing your request. Please try again later.",
-            ephemeral: true,
-          });
-          return;
-        }
-
-        // Get the label of the selected pet type
-        let selectedPetLabel;
-        if (selectedPetType === "1") {
-          selectedPetLabel = "Dog";
-        } else if (selectedPetType === "2") {
-          selectedPetLabel = "Cat";
-        } else if (selectedPetType === "3") {
-          selectedPetLabel = "Red Panda";
-        }
-        console.log("Selected Pet Label:", selectedPetLabel);
-
-        interaction.client.userStates[interaction.user.id] = {
-          state: "awaitingPetName",
-          selectedPetLabel,
-        };
-
-        // Now, you can initiate and show the modal for naming the pet with the selected pet type's label
-        const petNameInput = new TextInputBuilder()
-          .setLabel("Pet Name")
-          .setCustomId("petNameInput")
-          .setPlaceholder(
-            `Enter your ${selectedPetLabel}'s name. Max 25 chars and no more than 2 spaces are permitted.`
+      try {
+        await userModel
+          .findOneAndUpdate(
+            { userId: interaction.user.id },
+            { petType: selectedPetType },
+            { upsert: true }
           )
-          .setStyle("Short")
-          .setRequired(true);
-
-        const petNameActionRow = new ActionRowBuilder().addComponents(
-          petNameInput
-        );
-
-        const modal = new ModalBuilder()
-          .setTitle(`Name Your ${selectedPetLabel}`)
-          .setCustomId("namePetModal")
-          .addComponents(petNameActionRow);
-
-        // Show the modal
-        console.log("Showing Modal...");
-        await interaction.showModal(modal);
+          .exec();
+        console.log("Saved Pet Type to Database:", selectedPetType);
+      } catch (error) {
+        console.error("Error updating pet type:", error);
+        interaction.reply({
+          content:
+            "An error occurred while processing your request. Please try again later.",
+          ephemeral: true,
+        });
+        return;
       }
-    });
+
+      // Get the label of the selected pet type
+      let selectedPetLabel;
+      if (selectedPetType === "1") {
+        selectedPetLabel = "Dog";
+      } else if (selectedPetType === "2") {
+        selectedPetLabel = "Cat";
+      } else if (selectedPetType === "3") {
+        selectedPetLabel = "Red Panda";
+      }
+      console.log("Selected Pet Label:", selectedPetLabel);
+
+      interaction.client.userStates[interaction.user.id] = {
+        state: "awaitingPetName",
+        selectedPetLabel,
+      };
+
+      // Now, you can initiate and show the modal for naming the pet with the selected pet type's label
+      const petNameInput = new TextInputBuilder()
+        .setLabel("Pet Name")
+        .setCustomId("petNameInput")
+        .setPlaceholder(
+          `Enter your ${selectedPetLabel}'s name. Max 25 chars and no more than 2 spaces are permitted.`
+        )
+        .setStyle("Short")
+        .setRequired(true);
+
+      const petNameActionRow = new ActionRowBuilder().addComponents(
+        petNameInput
+      );
+
+      const modal = new ModalBuilder()
+        .setTitle(`Name Your ${selectedPetLabel}`)
+        .setCustomId("namePetModal")
+        .addComponents(petNameActionRow);
+
+      // Show the modal
+      console.log("Showing Modal...");
+      await interaction.showModal(modal);
+    }
   },
 };
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,62 +1,54 @@
 const userModel = require("../util/Models/userModel.js");
 
 module.exports = async (client, interaction) => {
-  const userDb = await client.database.getUser(interaction.user.id, true);
-  userModel.findOne({ userId: interaction.user.id }).then(async (result) => {
+  try {
+    const userDb = await client.database.getUser(interaction.user.id, true);
+    const result = await userModel.findOne({ userId: interaction.user.id });
     if (!result) {
       await userModel.create({
         userId: interaction.user.id,
       });
     }
-  });
 
-  if (interaction.isChatInputCommand()) {
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-      command.execute(interaction, client, userDb);
-    } catch (err) {
-      if (err) console.error(err);
-      interaction.reply({
-        content: "An error occurred while executing that command.",
-        ephemeral: true,
-      });
+    if (interaction.isChatInputCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (command) {
+        await command.execute(interaction, client, userDb);
+      }
+    } else if (interaction.isButton()) {
+      if (interaction.message.interaction.user.id !== interaction.user.id) {
+        return interaction.reply({
+          content: `This button does not belong to you it belongs to ${interaction.message.interaction.user.username}`,
+          ephemeral: true,
+        });
+      }
+      const button = client.buttons.get(interaction.customId);
+      if (button) {
+        await button.execute(interaction, client, userDb);
+      }
+    } else if (interaction.isStringSelectMenu()) {
+      const selectMenu = client.selectMenus.get(interaction.customId);
+      if (selectMenu) {
+        await selectMenu.execute(interaction, client, userDb);
+      }
+    } else {
+      if (interaction.message.interaction.user.id !== interaction.user.id) {
+        return interaction.reply({
+          content: `This interaction does not belong to you it belongs to ${interaction.message.interaction.user.username}`,
+          ephemeral: true,
+        });
+      } else if (interaction.isModalSubmit()) {
+        const modal = client.modals.get(interaction.customId);
+        if (modal) {
+          await modal.execute(interaction, client, userDb);
+        }
+      }
     }
-  } else if (interaction.isButton()) {
-    if (interaction.message.interaction.user.id !== interaction.user.id)
-      return interaction.reply({
-        content:
-          "This button does not belong to you it belongs to " +
-          interaction.message.interaction.user.username,
-        ephemeral: true,
-      });
-    const button = client.buttons.get(interaction.customId);
-    try {
-      button.execute(interaction, client, userDb);
-    } catch (err) {
-      if (err) console.error(err);
-      interaction.reply({
-        content: "An error occurred while trying to execute this button.",
-        ephemeral: true,
-      });
-    }
-  } else if (interaction.isButton()) {
-    if (interaction.message.interaction.user.id !== interaction.user.id)
-      return interaction.reply({
-        content:
-          "This button does not belong to you it belongs to " +
-          interaction.message.interaction.user.username,
-        ephemeral: true,
-      });
-    const button = client.buttons.get(interaction.customId);
-    try {
-      button.execute(interaction, client, userDb);
-    } catch (err) {
-      if (err) console.error(err);
-      interaction.reply({
-        content: "An error occurred while trying to execute this button.",
-        ephemeral: true,
-      });
-    }
+  } catch (err) {
+    console.error(err);
+    interaction.reply({
+      content: "An error occurred while executing that action.",
+      ephemeral: true,
+    });
   }
 };
