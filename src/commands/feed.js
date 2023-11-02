e71276e5903f610b6040a82636ed0b0f22e8cc7b
Provant <t6@live.com.au>
2023-11-02 12:54:26 +1000

    Add feeding and drinking timestamps and disable buttons based on time since last action.

--- src/commands/feed.js
+++ src/commands/feed.js
@@ -6,6 +6,7 @@ const {
 } = require("discord.js");
 const userModel = require("../util/Models/userModel");
 const speechBubbles = require("../data/speechbubbles.json");
+const timeStamp = require("../util/timeStamp");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -22,34 +23,92 @@ module.exports = {
 
     const petName = userDb.petName ? userDb.petName : "Your pet";
 
-    // Create buttons for feeding food and water
+    // Get the timestamps of the last few feeding and drinking actions
+    const lastFedTimes = userDb.actionTimestamps.lastFed;
+    const lastDrankTimes = userDb.actionTimestamps.lastDrank;
+
+    // Determine the time since the last actions
+    const timeSinceLastFed =
+      lastFedTimes.length > 0
+        ? Date.now() - new Date(lastFedTimes.slice(-1)[0]).getTime()
+        : Infinity;
+    const timeSinceLastDrank =
+      lastDrankTimes.length > 0
+        ? Date.now() - new Date(lastDrankTimes.slice(-1)[0]).getTime()
+        : Infinity;
+
+    // Determine the message to show based on the time since the last actions
+    let description = `Hello there! It seems like ${petName} is ready for a meal or a drink. What would you like to give them? 
+    \nProviding a balanced diet of food and water will keep ${petName} happy and healthy.
+    \nChoose an option below to feed or hydrate your adorable companion! 🍲💧`;
+
+    if (timeSinceLastFed < timeStamp.tenMinutes() && lastFedTimes.length >= 3) {
+      const timeRemaining = Math.ceil(
+        (timeStamp.tenMinutes() - timeSinceLastFed) / 60000
+      );
+      if (userDb.thirst < 20) {
+        description = `${petName} is still trying to digest from the last feed and isn't thirsty either. Try again in ${timeRemaining} minutes.`;
+      } else {
+        description = `${petName} is still trying to digest from the last feed. Try again in ${timeRemaining} minutes. They might want some water though! 💧`;
+      }
+    } else if (userDb.hunger < 20 && userDb.thirst >= 20) {
+      description = `${petName} isn't ready to eat, but they are thirsty.`;
+    } else if (userDb.thirst < 20 && userDb.hunger >= 20) {
+      description = `${petName} isn't thirsty, but they are ready to eat.`;
+    } else if (userDb.hunger < 20 && userDb.thirst < 20) {
+      description = `${petName} isn't hungry or thirsty right now.`;
+    }
+
+    // Check if pet is sick and if feeding fails
+    if (userDb.isSick) {
+      const randomChance = Math.random();
+      if (randomChance < 0.6) {
+        const feedFailEmbed = new EmbedBuilder()
+          .setColor("#9e38fe")
+          .setTitle("Oh no!")
+          .setDescription(
+            `${petName} isn't feeling well, they don't want to eat or drink right now.`
+          )
+          .setTimestamp();
+
+        await interaction.reply({
+          embeds: [feedFailEmbed],
+        });
+        return;
+      }
+    }
+
     const feedFoodButton = new ButtonBuilder()
       .setCustomId("feedFood")
       .setLabel("Give Food")
       .setStyle("Primary")
-      .setEmoji("🍽️");
+      .setEmoji("🍽️")
+      .setDisabled(
+        userDb.hunger < 20 ||
+          (timeSinceLastFed < timeStamp.tenMinutes() &&
+            lastFedTimes.length >= 3)
+      ); // disable the button based on these conditions
 
     const feedWaterButton = new ButtonBuilder()
       .setCustomId("feedWater")
       .setLabel("Give Water")
       .setStyle("Primary")
-      .setEmoji("💧");
+      .setEmoji("💧")
+      .setDisabled(
+        userDb.thirst < 20 ||
+          (timeSinceLastDrank < timeStamp.tenMinutes() &&
+            lastDrankTimes.length >= 3)
+      );
 
-    // Create an ActionRow to house the buttons
     const actionRow = new ActionRowBuilder().addComponents(
       feedFoodButton,
       feedWaterButton
     );
 
-    // Create an embed
     const embed = new EmbedBuilder()
       .setColor("#0099ff")
       .setTitle(`Feed ${petName}`)
-      .setDescription(
-        `Hello there! It seems like ${petName} is ready for a meal or a drink. What would you like to give them? 
-        \nProviding a balanced diet of food and water will keep ${petName} happy and healthy.
-        \nChoose an option below to feed or hydrate your adorable companion! 🍲💧`
-      );
+      .setDescription(description);
 
     // Reply with the embed and buttons
     await interaction.reply({
