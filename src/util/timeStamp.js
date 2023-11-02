a4be3ec1d67077225358d00a6efbedf8331b17dd
Provant <t6@live.com.au>
2023-11-02 11:45:20 +1000

    add additional timestamp variables

--- src/util/timeStamp.js
+++ src/util/timeStamp.js
@@ -1,4 +1,31 @@
 module.exports = {
+  // calculate time
+  oneMinute: () => 1 * 60 * 1000, // 1 minute = 60000 milliseconds
+  fiveMinutes: () => 5 * 60 * 1000, // 5 minutes = 300000 milliseconds
+  tenMinutes: () => 10 * 60 * 1000, // 10 minutes = 600000 milliseconds
+  fifteenMinutes: () => 15 * 60 * 1000, // 15 minutes = 900000 milliseconds
+  thirtyMinutes: () => 30 * 60 * 1000, // 30 minutes = 1800000 milliseconds
+  oneHour: () => 1 * 60 * 60 * 1000, // 1 hour = 3600000 milliseconds
+  twoHours: () => 2 * 60 * 60 * 1000, // 2 hours = 7200000 milliseconds
+  threeHours: () => 3 * 60 * 60 * 1000, // 3 hours = 10800000 milliseconds
+  fourHours: () => 4 * 60 * 60 * 1000, // 4 hours = 14400000 milliseconds
+  fiveHours: () => 5 * 60 * 60 * 1000, // 5 hours = 18000000 milliseconds
+  sixHours: () => 6 * 60 * 60 * 1000, // 6 hours = 21600000 milliseconds
+  sevenHours: () => 7 * 60 * 60 * 1000, // 7 hours = 25200000 milliseconds
+  eightHours: () => 8 * 60 * 60 * 1000, // 8 hours = 28800000 milliseconds
+  nineHours: () => 9 * 60 * 60 * 1000, // 9 hours = 32400000 milliseconds
+  tenHours: () => 10 * 60 * 60 * 1000, // 10 hours = 36000000 milliseconds
+  elevenHours: () => 11 * 60 * 60 * 1000, // 11 hours = 39600000 milliseconds
+  twelveHours: () => 12 * 60 * 60 * 1000, // 12 hours = 43200000 milliseconds
+  oneDay: () => 24 * 60 * 60 * 1000, // 1 day = 86400000 milliseconds
+  twoDays: () => 2 * 24 * 60 * 60 * 1000, // 2 days = 172800000 milliseconds
+  threeDays: () => 3 * 24 * 60 * 60 * 1000, // 3 days = 259200000 milliseconds
+  fourDays: () => 4 * 24 * 60 * 60 * 1000, // 4 days = 345600000 milliseconds
+  fiveDays: () => 5 * 24 * 60 * 60 * 1000, // 5 days = 432000000 milliseconds
+  sixDays: () => 6 * 24 * 60 * 60 * 1000, // 6 days = 518400000 milliseconds
+  oneWeek: () => 7 * 24 * 60 * 60 * 1000, // 1 week = 604800000 milliseconds
+
+  // past time
   oneMinuteAgo: () => Date.now() - 1 * 60 * 1000,
   fiveMinutesAgo: () => Date.now() - 5 * 60 * 1000,
   tenMinutesAgo: () => Date.now() - 10 * 60 * 1000,
