8661988203741c4915a85ce923ff705adbaaa3d0
Provant <t6@live.com.au>
2023-11-03 22:26:48 +1000

    Add checkPetStatus function and remove walk and run subcommands

--- src/commands/play.js
+++ src/commands/play.js
@@ -8,6 +8,7 @@ const userModel = require("../util/Models/userModel");
 const speechBubbles = require("../data/speechbubbles.json");
 const timeStamp = require("../util/timeStamp");
 const variables = require("../data/variableNames");
+const checkPetStatus = require("../util/eventChecks");
 
 function getPetSounds(userDb) {
   const petTypeStrMap = {
@@ -41,12 +42,6 @@ module.exports = {
       subcommand
         .setName("cuddle")
         .setDescription("Cuddle with your pet to increase bonding")
-    )
-    .addSubcommand((subcommand) =>
-      subcommand.setName("walk").setDescription("Take your pet for a walk")
-    )
-    .addSubcommand((subcommand) =>
-      subcommand.setName("run").setDescription("Take your pet for a run")
     ),
 
   async execute(interaction) {
@@ -66,6 +61,11 @@ module.exports = {
       return;
     }
 
+    const canContinue = await checkPetStatus(interaction, userDb, petName, now);
+    if (!canContinue) {
+      return;
+    }
+
     switch (subcommand) {
       case "pat":
         await handlePat(interaction, userDb, petName, now, randomPetSound);
@@ -73,13 +73,8 @@ module.exports = {
       case "cuddle":
         await handleCuddle(interaction, userDb, petName, now, randomPetSound);
         break;
-      case "walk":
-        await handleWalk(interaction, userDb, petName, now, randomPetSound);
-        break;
-      case "run":
-        await handleRun(interaction, userDb, petName, now, randomPetSound);
-        break;
     }
+
     await userDb.save();
   },
 };
@@ -92,7 +87,7 @@ async function handlePat(interaction, userDb, petName, now, randomPetSound) {
     return;
   }
 
-  const recentPats = userDb.actionTimestamps.lastPat.filter(
+  const recentPats = userDb.actionTimeStamp.lastPat.filter(
     (patTime) => new Date(patTime).getTime() > now - timeStamp.tenMinutes()
   );
 
@@ -103,18 +98,12 @@ async function handlePat(interaction, userDb, petName, now, randomPetSound) {
     return;
   }
 
-  // Push the current timestamp to the lastPat array
-  userDb.actionTimestamps.lastPat.push(now);
-  // Keep only the last 5 timestamps
-  if (userDb.actionTimestamps.lastPat.length > 5) {
-    userDb.actionTimestamps.lastPat.shift();
-  }
+  userDb.actionTimeStamp.lastPat.push(now);
+  userDb.actionTimeStamp.lastPat = userDb.actionTimeStamp.lastPat.slice(-5);
 
-  // Calculate the new happiness level
   const happinessIncrease = Math.floor(Math.random() * 25) + 1;
   userDb.happiness = Math.min(userDb.happiness + happinessIncrease, 100);
 
-  // Save the updated userDb
   await userDb.save();
 
   const patEmbed = new EmbedBuilder()
@@ -139,7 +128,7 @@ async function handlePat(interaction, userDb, petName, now, randomPetSound) {
 
 async function handleCuddle(interaction, userDb, petName, now, randomPetSound) {
   // Retrieve the last cuddle timestamp from the database
-  const lastCuddledArray = userDb.actionTimestamps.lastCuddled;
+  const lastCuddledArray = userDb.actionTimeStamp.lastCuddled;
   const lastCuddled =
     lastCuddledArray.length > 0
       ? new Date(lastCuddledArray[lastCuddledArray.length - 1])
@@ -157,9 +146,9 @@ async function handleCuddle(interaction, userDb, petName, now, randomPetSound) {
   }
 
   // Add the current timestamp to the lastCuddled array and remove the oldest if necessary
-  userDb.actionTimestamps.lastCuddled.push(now);
-  if (userDb.actionTimestamps.lastCuddled.length > 5) {
-    userDb.actionTimestamps.lastCuddled.shift();
+  userDb.actionTimeStamp.lastCuddled.push(now);
+  if (userDb.actionTimeStamp.lastCuddled.length > 5) {
+    userDb.actionTimeStamp.lastCuddled.shift();
   }
 
   // Calculate the changes in affection and happiness
@@ -178,7 +167,9 @@ async function handleCuddle(interaction, userDb, petName, now, randomPetSound) {
   const cuddleEmbed = new EmbedBuilder()
     .setColor("#9e38fe")
     .setTitle("Cuddle Time!")
-    .setDescription(`You cuddled with ${petName}. It looks very happy!`)
+    .setDescription(
+      `${randomPetSound}! You cuddled with ${petName}. It looks very happy!`
+    )
     .addFields(
       {
         name: "Energy",
@@ -200,178 +191,5 @@ async function handleCuddle(interaction, userDb, petName, now, randomPetSound) {
 
   await interaction.reply({
     embeds: [cuddleEmbed],
-  });
-}
-
-async function handleWalk(interaction, userDb, petName, now, randomPetSound) {
-  // Retrieve the last walked and ran timestamps
-  const lastWalkedArray = userDb.actionTimestamps.lastWalked;
-  const lastWalked =
-    lastWalkedArray.length > 0
-      ? new Date(lastWalkedArray[lastWalkedArray.length - 1])
-      : null;
-  const lastRanArray = userDb.actionTimestamps.lastRan;
-  const lastRan =
-    lastRanArray.length > 0
-      ? new Date(lastRanArray[lastRanArray.length - 1])
-      : null;
-
-  // Check if the pet has been walked or ran in the last hour
-  if (
-    (lastWalked && now - lastWalked.getTime() < timeStamp.oneHour()) ||
-    (lastRan && now - lastRan.getTime() < timeStamp.oneHour())
-  ) {
-    const timeRemaining = Math.max(
-      lastWalked ? timeStamp.oneHour() - (now - lastWalked.getTime()) : 0,
-      lastRan ? timeStamp.oneHour() - (now - lastRan.getTime()) : 0
-    );
-    const remainingMinutes = Math.ceil(timeRemaining / (60 * 1000));
-    await interaction.reply({
-      content: `Please wait ${remainingMinutes} minute(s). ${petName} is still tired from the last activity.`,
-    });
-    return;
-  }
-
-  // Add the current timestamp to the lastWalked array
-  userDb.actionTimestamps.lastWalked.push(now);
-  if (userDb.actionTimestamps.lastWalked.length > 5) {
-    userDb.actionTimestamps.lastWalked.shift();
-  }
-
-  // Calculate the changes in energy, affection, and happiness
-  const energyDecreasePercentage = Math.random() * 7 + 5;
-  userDb.energy = Math.max(
-    userDb.energy - userDb.energy * (energyDecreasePercentage / 100),
-    0
-  );
-
-  const affectionIncrease = userDb.petType === 2 ? -2 : 2;
-  userDb.affection = Math.max(
-    Math.min(userDb.affection + affectionIncrease, 100),
-    0
-  );
-
-  const happinessIncrease = userDb.petType === 2 ? -2 : 2;
-  userDb.happiness = Math.max(
-    Math.min(userDb.happiness + happinessIncrease, 100),
-    0
-  );
-
-  // Save the updated userDb
-  await userDb.save();
-
-  const walkEmbed = new EmbedBuilder()
-    .setColor("#9e38fe")
-    .setTitle(`${petName} went for a walk!`)
-    .setDescription(
-      `It was refreshing and enjoyable. ${petName} is looking happier!`
-    )
-    .addFields(
-      {
-        name: "Energy",
-        value: `${variables.getEnergy(userDb.energy)}`,
-        inline: true,
-      },
-      {
-        name: "Affection",
-        value: `${variables.getAffection(userDb.affection)}`,
-        inline: true,
-      },
-      {
-        name: "Happiness",
-        value: `${variables.getHappiness(userDb.happiness)}`,
-        inline: true,
-      }
-    )
-    .setTimestamp();
-
-  await interaction.reply({
-    embeds: [walkEmbed],
-  });
-}
-
-async function handleRun(interaction, userDb, petName, now, randomPetSound) {
-  // Retrieve the last walked and ran timestamps
-  const lastWalkedArray = userDb.actionTimestamps.lastWalked;
-  const lastWalked =
-    lastWalkedArray.length > 0
-      ? new Date(lastWalkedArray[lastWalkedArray.length - 1])
-      : null;
-  const lastRanArray = userDb.actionTimestamps.lastRan;
-  const lastRan =
-    lastRanArray.length > 0
-      ? new Date(lastRanArray[lastRanArray.length - 1])
-      : null;
-
-  // Check if the pet has been walked or ran in the last hour
-  if (
-    (lastWalked && now - lastWalked.getTime() < timeStamp.oneHour()) ||
-    (lastRan && now - lastRan.getTime() < timeStamp.oneHour())
-  ) {
-    const timeRemaining = Math.max(
-      lastWalked ? timeStamp.oneHour() - (now - lastWalked.getTime()) : 0,
-      lastRan ? timeStamp.oneHour() - (now - lastRan.getTime()) : 0
-    );
-    const remainingMinutes = Math.ceil(timeRemaining / (60 * 1000));
-    await interaction.reply({
-      content: `Please wait ${remainingMinutes} minute(s). ${petName} is still recovering from the last activity.`,
-    });
-    return;
-  }
-
-  // Add the current timestamp to the lastRan array
-  userDb.actionTimestamps.lastRan.push(now);
-  if (userDb.actionTimestamps.lastRan.length > 5) {
-    userDb.actionTimestamps.lastRan.shift();
-  }
-
-  const energyDecreasePercentage = Math.random() * 10 + 15;
-  userDb.energy = Math.max(
-    userDb.energy - userDb.energy * (energyDecreasePercentage / 100),
-    0
-  );
-
-  const affectionChange = userDb.petType === 2 ? -1 : 1;
-  userDb.affection = Math.max(
-    Math.min(userDb.affection + affectionChange, 100),
-    0
-  );
-
-  const happinessChange = userDb.petType === 2 ? -2 : 2;
-  userDb.happiness = Math.max(
-    Math.min(userDb.happiness + happinessChange, 100),
-    0
-  );
-
-  // Save the updated userDb
-  await userDb.save();
-
-  const runEmbed = new EmbedBuilder()
-    .setColor("#9e38fe")
-    .setTitle(`${petName} went for a run!`)
-    .setDescription(
-      `${randomPetSound}! That was intense! ${petName} is now quite tired but feeling accomplished.`
-    )
-    .addFields(
-      {
-        name: "Energy",
-        value: `${variables.getEnergy(userDb.energy)}`,
-        inline: true,
-      },
-      {
-        name: "Affection",
-        value: `${variables.getAffection(userDb.affection)}`,
-        inline: true,
-      },
-      {
-        name: "Happiness",
-        value: `${variables.getHappiness(userDb.happiness)}`,
-        inline: true,
-      }
-    )
-    .setTimestamp();
-
-  await interaction.reply({
-    embeds: [runEmbed],
   });
 }
