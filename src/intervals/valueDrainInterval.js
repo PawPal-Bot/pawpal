a9986591dbdce268c4999333f89afd87a97b0aff
Provant <t6@live.com.au>
2023-11-03 22:27:05 +1000

    Adjust pet drain rates and add sick status handling

--- src/intervals/valueDrainInterval.js
+++ src/intervals/valueDrainInterval.js
@@ -7,67 +7,84 @@ module.exports = async function valueDrainInterval() {
     const users = await userModel.find();
 
     let operations = users.map((user) => {
-      let happinessDrainRate = 1;
-      let affectionDrainRate = 0.5;
-      let energyDrainRate = 1;
-      let hungerDrainRate = 1;
-      let thirstDrainRate = 1;
-      let cleanlinessDrainRate = 1;
-      let sleepLevelDrainRate = 1;
+      let happinessDrainRate = 3;
+      let affectionDrainRate = 3;
+      let energyDrainRate = 3;
+      let hungerDrainRate = 3;
+      let thirstDrainRate = 3;
+      let cleanlinessDrainRate = 3;
+      let sleepLevelDrainRate = 4;
 
-      // Check action timestamps and adjust drain rates
-      const { actionTimestamps, petType } = user;
+      const lastVetVisitTime = user.actionTimeStamp.lastVetVisit?.length
+        ? new Date(
+            user.actionTimeStamp.lastVetVisit[
+              user.actionTimeStamp.lastVetVisit.length - 1
+            ]
+          ).getTime()
+        : 0;
+      const timeSinceLastVetVisit = Date.now() - lastVetVisitTime;
+      const sixHoursPassed = timeSinceLastVetVisit >= timeStamp.sixHours();
+
+      if (user.isSick && !sixHoursPassed) {
+        happinessDrainRate = 6;
+        affectionDrainRate = 0;
+        energyDrainRate = 0;
+        hungerDrainRate = 0;
+        thirstDrainRate = 0;
+        cleanlinessDrainRate = 0;
+        sleepLevelDrainRate = 0;
+      } else if (user.isSick && sixHoursPassed) {
+        user.isSick = false;
+      }
+
+      const { actionTimeStamp, petType } = user;
       if (
-        actionTimestamps.lastPat > timeStamp.twoHoursAgo ||
-        actionTimestamps.lastCuddled > timeStamp.twoHoursAgo ||
-        actionTimestamps.lastPlayed > timeStamp.twoHoursAgo ||
-        actionTimestamps.lastCleaned > timeStamp.twoHoursAgo
+        actionTimeStamp.lastPat > timeStamp.twoHoursAgo() ||
+        actionTimeStamp.lastCuddled > timeStamp.twoHoursAgo() ||
+        actionTimeStamp.lastPlayed > timeStamp.twoHoursAgo() ||
+        actionTimeStamp.lastCleaned > timeStamp.twoHoursAgo()
       ) {
         happinessDrainRate /= 2;
         affectionDrainRate /= 2;
       }
 
       if (petType !== 2) {
-        // if the pet is not a cat, increase values. if it is a cat, decrease values further.
         if (
-          actionTimestamps.lastRan > timeStamp.twoHoursAgo ||
-          actionTimestamps.lastWalk > timeStamp.twoHoursAgo
+          actionTimeStamp.lastRan > timeStamp.twoHoursAgo() ||
+          actionTimeStamp.lastWalked > timeStamp.twoHoursAgo()
         ) {
           happinessDrainRate += 1;
         }
       }
 
-      // if pets cleanliness is less than 25 and greater than 1, there is a 50% chance of becoming sick
-      if (user.cleanliness < 25 && user.cleanliness > 1) {
-        const randomChance = Math.random();
-        if (randomChance < 0.5) {
-          user.isSick = true;
-        }
-      }
+      let update = {
+        happiness: Math.max(user.happiness - happinessDrainRate, 0),
+        affection: Math.max(user.affection - affectionDrainRate, 0),
+        energy: user.isSick
+          ? user.energy
+          : Math.max(user.energy - energyDrainRate, 0),
+        hunger: user.isSick
+          ? user.hunger
+          : Math.max(user.hunger - hungerDrainRate, 0),
+        thirst: user.isSick
+          ? user.thirst
+          : Math.max(user.thirst - thirstDrainRate, 0),
+        cleanliness: user.isSick
+          ? user.cleanliness
+          : Math.max(user.cleanliness - cleanlinessDrainRate, 0),
+        sleepLevel: user.isSick
+          ? user.sleepLevel
+          : Math.max(user.sleepLevel - sleepLevelDrainRate, 0),
+      };
 
-      // if the pet is sick, increase the drain rates
-      if (user.isSick) {
-        happinessDrainRate = 6;
-        affectionDrainRate = 6;
-        energyDrainRate = 6;
-        healthDrainRate = 6;
-        hungerDrainRate = 6;
-        thirstDrainRate = 6;
-        cleanlinessDrainRate = 6;
+      if (user.isSick && sixHoursPassed) {
+        update.isSick = false;
       }
 
       return {
         updateOne: {
           filter: { _id: user._id },
-          update: {
-            happiness: Math.max(user.happiness - happinessDrainRate, 0),
-            energy: Math.max(user.energy - energyDrainRate, 0),
-            hunger: Math.max(user.hunger - hungerDrainRate, 0),
-            thirst: Math.max(user.thirst - thirstDrainRate, 0),
-            cleanliness: Math.max(user.cleanliness - cleanlinessDrainRate, 0),
-            sleepLevel: Math.max(user.sleepLevel - sleepLevelDrainRate, 0),
-            affection: Math.max(user.affection - affectionDrainRate, 0),
-          },
+          update: update,
         },
       };
     });
