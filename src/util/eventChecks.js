ab4ae1503984cdd8ab03cdf31d5722ab361b66da
Provant <t6@live.com.au>
2023-11-05 16:24:46 +1000

    Add sleep interruption handling to checkPetStatus function

--- src/util/eventChecks.js
+++ src/util/eventChecks.js
@@ -21,7 +21,48 @@ async function checkPetStatus(interaction, userDb, petName, now) {
       return false;
     }
 
-    if (userDb.isAsleep) {
+    const sleepUntilTime = userDb.sleepUntil ? new Date(userDb.sleepUntil).getTime() : null;
+    
+    if (userDb.isAsleep && (sleepUntilTime && now < sleepUntilTime)) {
+      const lastSleptTime = new Date(userDb.actionTimeStamp.lastSlept[0]).getTime();
+      const actualSleepDuration = now - lastSleptTime;
+
+      const maxSleepDuration = 4 * 60 * 60 * 1000; 
+      const actualRecoveryPercentage = (actualSleepDuration / maxSleepDuration);
+      
+      const actualEnergyGain = Math.round(actualRecoveryPercentage * 100);
+      const actualSleepLevelGain = Math.round(actualRecoveryPercentage * 100);
+      
+      userDb.energy = Math.min(userDb.energy + actualEnergyGain, 100);
+      userDb.sleepLevel = Math.min(userDb.sleepLevel + actualSleepLevelGain, 100);
+
+      userDb.energy = Math.max(userDb.energy - 5, 0);
+      userDb.sleepLevel = Math.max(userDb.sleepLevel - 5, 0);
+
+      userDb.isAsleep = false;
+      userDb.sleepUntil = null;
+
+      await userDb.save();
+
+      if (interaction.deferred || interaction.replied) {
+        await interaction.followUp(
+          `${petName} was woken up early from their slumber and is a bit groggy. Energy and sleep level have decreased due to the interrupted sleep.`
+        );
+      } else {
+        await interaction.reply(
+          `${petName} was woken up early from their slumber and is a bit groggy. Energy and sleep level have decreased due to the interrupted sleep.`
+        );
+      }
+      return false;
+    }
+    
+    const hasSleepUntilPassed = sleepUntilTime ? now > sleepUntilTime : false;
+
+    if (hasSleepUntilPassed) {
+      userDb.isAsleep = false;
+      userDb.sleepUntil = null;
+      await userDb.save();
+    } else if (userDb.isAsleep) {
       userDb.energy = Math.max(userDb.energy - userDb.energy * 0.05, 0);
       userDb.happiness = Math.max(
         userDb.happiness - userDb.happiness * 0.05,
