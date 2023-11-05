ebfb140029c6e56a351764ac5a20889d5e522ac7
Provant <t6@live.com.au>
2023-11-05 16:24:26 +1000

    Remove unnecessary comments

--- src/intervals/startIntervals.js
+++ src/intervals/startIntervals.js
@@ -4,8 +4,8 @@ const ageUpdateInterval = require("./ageUpdateInterval");
 module.exports = (client) => {
   console.log("Starting intervals...");
 
-  setInterval(valueDrainInterval, 3600000); // Start the value drain interval
-  setInterval(ageUpdateInterval, 3600000); // Start the age update interval
+  setInterval(valueDrainInterval, 3600000);
+  setInterval(ageUpdateInterval, 3600000); 
 
   console.log("Intervals started.");
 };
