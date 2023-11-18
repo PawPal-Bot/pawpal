c6975a7971e7cfe2c4402f03a98a5e6de3d65dfa
Provant <t6@live.com.au>
2023-11-18 15:32:03 +1000

    Refactor devSupport modal

--- src/components/modals/devSupport.js
+++ src/components/modals/devSupport.js
@@ -1,22 +1,35 @@
-const { ModalSubmitInteraction } = require('discord.js');
-const ExtendedClient = require('../../class/ExtendedClient');
+const { ModalSubmitInteraction, WebhookClient, EmbedBuilder, Colors } = require("discord.js");
+const ExtendedClient = require("../../class/ExtendedClient");
 
 module.exports = {
-    customId: 'dev-support-modal',
-    /**
-     * 
-     * @param {ExtendedClient} client 
-     * @param {ModalSubmitInteraction} interaction 
-     */
-    run: async (client, interaction) => {
-        const guildName = interaction.fields.getTextInputValue('guild_name_input');
-        const guildId = interaction.fields.getTextInputValue('guild_id_input');
-        const issue = interaction.fields.getTextInputValue('issue_input');
-        const email = interaction.fields.getTextInputValue('email_input');
+  customId: "dev-support-modal",
+  /**
+   * @param {ExtendedClient} client
+   * @param {ModalSubmitInteraction} interaction
+   */
+  run: async (client, interaction) => {
+    const guildName = interaction.fields.getTextInputValue("guild_name_input");
+    const guildId = interaction.fields.getTextInputValue("guild_id_input");
+    const issue = interaction.fields.getTextInputValue("issue_input");
+    const email = interaction.fields.getTextInputValue("email_input");
 
-        const server = client.guilds.cache.get('798420885258240060');
-        const channel = server.channels.cache.get('1166611710786932746');
-        await channel.send(`New support request:\n**Guild Name:** ${guildName}\n**Guild ID:** ${guildId}\n**Issue:** ${issue}\n**Email:** ${email}`);
-        await interaction.reply({ content: 'Thank you for submitting your request. Our team will get back to you shortly.', ephemeral: true });
+    const supportWebhookId = process.env.SUPPORT_WEBHOOK_ID;
+    const supportWebhookToken = process.env.SUPPORT_WEBHOOK_TOKEN;
+
+    if (supportWebhookId && supportWebhookToken) {
+      const webhookClient = new WebhookClient({ id: supportWebhookId, token: supportWebhookToken });
+
+      const embed = new EmbedBuilder()
+        .setColor(Colors.Blue) // Use the Colors enumeration for setting color
+        .setTitle("New Support Request")
+        .setDescription(`**Guild Name:** ${guildName}\n**Guild ID:** ${guildId}\n**Issue:** ${issue}\n**Email:** ${email}`)
+        .setTimestamp();
+
+      await webhookClient.send({ embeds: [embed] });
+      await interaction.reply({ content: "Thank you for submitting your request. Our team will get back to you shortly.", ephemeral: true });
+    } else {
+      console.error("Support webhook configuration not found.");
+      await interaction.reply({ content: "There was an error submitting your request. Please try again later.", ephemeral: true });
     }
+  },
 };
