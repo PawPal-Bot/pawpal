84422f658dcdfc462d78631aefee31ed23727544
Provant <t6@live.com.au>
2023-11-19 19:37:25 +1000

    Refactor guildCreate event handler

--- src/events/Guild/guildCreate.js
+++ src/events/Guild/guildCreate.js
@@ -1,25 +1,22 @@
 const { log } = require("../../functions");
 const GuildProfile = require("../../schemas/GuildSchema");
 const ExtendedClient = require("../../class/ExtendedClient");
-const { EmbedBuilder, WebhookClient } = require("discord.js");
+const { WebhookClient } = require("discord.js");
 
 module.exports = {
   event: "guildCreate",
   /**
    *
-   * @param {ExtendedClient} _
+   * @param {ExtendedClient} client
    * @param {import('discord.js').Guild} guild
    * @returns
    */
-  run: async (_, guild) => {
+  run: async (client, guild) => {
     try {
-      const newGuildData = new GuildProfile(
-        {
-          _id: guild.id,
-          guildName: guild.name,
-        },
-        { timestamps: true }
-      );
+      const newGuildData = new GuildProfile({
+        _id: guild.id,
+        guildName: guild.name,
+      }, { timestamps: true });
 
       await newGuildData.save();
 
@@ -33,15 +30,22 @@ module.exports = {
           id: guildActivityWebhookId,
           token: guildActivityWebhookToken,
         });
+        
 
-        const embed = new EmbedBuilder().setColor(0x00ff00).setTitle("New Guild Joined!").setDescription(`Bot has joined ${guild.name}`).setTimestamp();
+        const animalEmojis = ['🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐻', '🐼', '🐨', '🐯'];
+        const randomEmoji = animalEmojis[Math.floor(Math.random() * animalEmojis.length)];
 
-        await webhookClient.send({ embeds: [embed] });
+        const botName = client.user.username;        
+        const guildIconURL = guild.iconURL({ dynamic: true, size: 1024 });
+        const serverCount = client.guilds.cache.size;
+        const messageContent = `:green_circle: ${botName} has joined ${guild.name}. We are now in ${serverCount} servers. ${randomEmoji}`;
+
+    await webhookClient.send({ content: messageContent, username: guild.name, avatarURL: guildIconURL });
       } else {
-        console.error("Guild activity webhook configuration not found.");
+        log("Guild activity webhook configuration not found.", "error");
       }
     } catch (error) {
-      log(`Failed to create data for guild ID ${guild.id}: ${error}`, "error");
+      log(`Failed to handle guild create for ${guild.id}: ${error}`, "error");
     }
   },
 };
